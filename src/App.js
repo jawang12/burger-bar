64d9273a7935a3050998c1ebc66b700112857e0e
Jimmy Wang <jawang12@gmail.com>
2020-03-26 21:59:21 -0400

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import Main from './containers/Main/Main';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Login from './containers/Auth/Login';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
 import { connect } from 'react-redux';
 import Logout from './containers/Auth/Logout';
+import Spinner from './components/UI/Spinner/Spinner';
+
+const LazyLoadOrders = lazy(() => import('./containers/Orders/Orders'));
+const LazyLoadCheckout = lazy(() => import('./containers/Checkout/Checkout'));
+const LazyLoadLogin = lazy(() => import('./containers/Auth/Login'));
 
 class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/login" component={Login} />
+        <Route
+          path="/login"
+          render={() => (
+            <Suspense fallback={<Spinner />}>
+              <LazyLoadLogin />
+            </Suspense>
+          )}
+        />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
     );
 
     if (this.props.isAuthenticated) {
-      console.log('authenticated');
       routes = (
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route
+            path="/login"
+            render={() => (
+              <Suspense fallback={<Spinner />}>
+                <LazyLoadLogin />
+              </Suspense>
+            )}
+          />
+          <Route
+            path="/checkout"
+            render={() => (
+              <Suspense fallback={<Spinner />}>
+                <LazyLoadCheckout />
+              </Suspense>
+            )}
+          />
+          <Route
+            path="/orders"
+            render={() => (
+              <Suspense fallback={<Spinner />}>
+                <LazyLoadOrders />
+              </Suspense>
+            )}
+          />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import ReviewOrder from '../../components/Order/ReviewOrder/ReviewOrder';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, withRouter } from 'react-router-dom';
 import CustomerInfo from './CustomerInfo/CustomerInfo';
 import { connect } from 'react-redux';
 
 class Checkout extends Component {
-  componentDidMount() {
-    // const queryIterator = new URLSearchParams(this.props.location.search);
-    // const ingredients = {};
-    // for (let param of queryIterator) {
-    //   if (param[0] !== 'price') {
-    //     ingredients[param[0]] = param[1];
-    //   }
-    // }
-    // const price = +queryIterator.get('price');
-    // this.setState({ ingredients, price });
-  }
+  // componentDidMount() {
+  //   const queryIterator = new URLSearchParams(this.props.location.search);
+  //   const ingredients = {};
+  //   for (let param of queryIterator) {
+  //     if (param[0] !== 'price') {
+  //       ingredients[param[0]] = param[1];
+  //     }
+  //   }
+  //   const price = +queryIterator.get('price');
+  //   this.setState({ ingredients, price });
+  // }
 
   cancelCheckoutHandler = () => {
     this.props.history.goBack();
@@ -49,4 +49,4 @@ const mapStateToProps = state => ({
   ordering: state.orders.ordering
 });
 
-export default connect(mapStateToProps)(Checkout);
+export default withRouter(connect(mapStateToProps)(Checkout));
